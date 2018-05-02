622503a1a5245f8d1a65e3be0cba437342d9557d
Shozaburo Shimada <shozaburo@piccolo.co>
2018-05-02 19:04:17 +0900

    update

---
+++ DiabloUI/Assets/Scripts/Classes.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+
+}
+
+public interface Skill {
+
+}
+
+[System.Serializable]
+public abstract class SkillBase:Skill {
+
+    public string Name;
+    public Sprite SkillIcon;
+    public Characters BelongToWho;
+    public string Description;
+
+
+}
+
+public enum Characters {
+    
+    Barbarian, Crusader, DemonHunter, Monk, WitchDoctor, Wizard
+}
+
+[System.Serializable]
+public abstract class ActiveSkill : SkillBase {
+
+
+}
+
+[System.Serializable]
+public abstract class PassiveSkill : SkillBase {
+
+
+}
+
+[System.Serializable]
+public abstract class DHActiveSkill : ActiveSkill {
+
+
+}
+
+[System.Serializable]
+public abstract class DHPassiveSkill : PassiveSkill {
+
+
+}
+
+
+[System.Serializable]
+public class SpikeTrap : DHActiveSkill {
+
+}
