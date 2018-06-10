59d71ad4d40abb7615c6c3a21196d91259635512
Shozaburo Shimada <shozaburo.s@gmail.com>
2018-06-11 08:57:22 +0900

    update

---
+++ Unity/ClimbCloud/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody2D rigid2D;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space)){
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        //Right/Left
+        int key = 0;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        if (speedx < this.maxWalkSpeed) {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if (key != 0) {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+
+
+	}
+}
---
+++ Unity/TPS/Assets/Assets/Scripts/UnityChanDemo1.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanDemo1 : MonoBehaviour {
+
+    private Animator animator;
+    private Rigidbody rb;
+    bool f_ground;
+
+    public float thrust = 100.0f;
+    public float rlspeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (f_ground){
+            if (Input.GetKey("up")) { 
+                transform.position += transform.forward * 0.01f;
+                animator.SetBool("is_running", true);
+            }else{ 
+                animator.SetBool("is_running", false);
+            }
+
+            if (Input.GetKey("right")){
+                transform.Rotate(0, 10, 0);
+            }
+
+            if (Input.GetKey("left")){
+                transform.Rotate(0, -10, 0);
+            }
+
+            if (Input.GetKey(KeyCode.A)) {
+                Debug.Log("left");
+                //rb.AddForce(transform.right * (-1) * rlspeed);
+                transform.position += transform.right * -0.01f;
+            }
+
+            if (Input.GetKey(KeyCode.D)) {
+                Debug.Log("right");
+                //rb.AddForce(transform.right * (1) * rlspeed);
+                transform.position += transform.right * 0.01f;
+            }
+
+
+            if (Input.GetKey(KeyCode.Space)){
+                rb.AddForce(new Vector3(0, thrust, 0));
+                f_ground = false;
+                //Debug.Log("ground:false");
+                animator.SetBool("is_jumping", true);
+            } else {
+                animator.SetBool("is_jumping", false);
+            }
+        }
+	}
+
+    private void OnCollisionStay(Collision collision) {
+        f_ground = true;
+        //Debug.Log("ground:true");
+    }
+}
