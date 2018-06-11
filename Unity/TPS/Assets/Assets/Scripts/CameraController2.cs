febfb42604ae743756a98234e92c97276ae44de0
Shozaburo Shimada <shozaburo@piccolo.co>
2018-06-11 17:29:43 +0900

    implemant tps camera

---
+++ Unity/TPS/Assets/Assets/Scripts/CameraController2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//http://sasanon.hatenablog.jp/entry/2017/09/17/041612
+public class CameraController2 : MonoBehaviour {
+    [SerializeField] private Transform player;          //注視対象プレイヤー
+    [SerializeField] private float distance = 1.0f;    //注視プライヤーとカメラとの距離
+    [SerializeField] private Quaternion vRotation;      //カメラの垂直回転
+    [SerializeField] private Quaternion hRotation;      //カメラの水平回転
+    [SerializeField] private float turnSpeed = 10.0f;      //カメラの水平回転
+
+    // Use this for initialization
+    void Start () {
+        // Init Rotation
+        Debug.Log("Player rotation: " +player.rotation);
+        Debug.Log("Player position: " + player.position);
+        transform.rotation = player.rotation;
+        /*
+        vRotation = Quaternion.Euler(30, 0, 0); //垂直は30度見下ろし
+        hRotation = Quaternion.identity;        //水平は無回転
+        //hRotation = Quaternion.Euler(player.forward);
+        transform.rotation = hRotation * vRotation;
+        */
+        //Init Position
+        transform.position = player.position - transform.rotation * Vector3.forward * distance;
+        //transform.position = player.position - transform.rotation * player.forward * distance;
+    }
+	
+	void LateUpdate () {
+        //水平回転
+        //if (Input.GetMouseButton(0))
+            hRotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * turnSpeed, 0);
+
+        transform.rotation = hRotation * vRotation;
+
+        transform.position = player.position - transform.rotation * Vector3.forward * distance;
+    }
+}
--- Unity/TPS/Assets/Assets/Scripts/UnityChanDemo1.cs
+++ Unity/TPS/Assets/Assets/Scripts/UnityChanDemo1.cs
@@ -8,47 +8,65 @@ public class UnityChanDemo1 : MonoBehaviour {
     private Rigidbody rb;
     bool f_ground;
 
+    public Vector3 velocity;
     public float thrust = 100.0f;
-    public float rlspeed = 5.0f;
+    public float movespeed = 5.0f;
+
+    //For Rotation, http://corevale.com/unity/2346
+    public Vector2 rotationSpeed;
+    public bool reverse = false;
+    private Vector2 lastMousePosition;
+    private Vector2 newAngle = Vector2.zero;
+    Transform campos;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        Debug.Log("My position: " + transform.position);
+        //campos = transform.Find("CamPos").transform;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (f_ground){
-            if (Input.GetKey("up")) { 
-                transform.position += transform.forward * 0.01f;
-                animator.SetBool("is_running", true);
-            }else{ 
-                animator.SetBool("is_running", false);
-            }
 
-            if (Input.GetKey("right")){
-                transform.Rotate(0, 10, 0);
-            }
+    // Update is called once per frame
+    void Update() {
 
-            if (Input.GetKey("left")){
-                transform.Rotate(0, -10, 0);
+
+        if (f_ground) {
+
+            //Move
+            velocity = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W)) {
+                //transform.position += transform.forward * 0.01f;
+                velocity += transform.forward;
             }
 
             if (Input.GetKey(KeyCode.A)) {
-                Debug.Log("left");
-                //rb.AddForce(transform.right * (-1) * rlspeed);
-                transform.position += transform.right * -0.01f;
+                //transform.position += transform.right * -0.01f;
+                velocity -= transform.right;
+            }
+
+            if (Input.GetKey(KeyCode.S)) {
+                //transform.position += transform.right * -0.01f;
+                velocity -= transform.forward;
             }
 
             if (Input.GetKey(KeyCode.D)) {
-                Debug.Log("right");
-                //rb.AddForce(transform.right * (1) * rlspeed);
-                transform.position += transform.right * 0.01f;
+                //transform.position += transform.right * 0.01f;
+                velocity += transform.right;
             }
 
+            velocity = velocity.normalized * movespeed * Time.deltaTime;
 
-            if (Input.GetKey(KeyCode.Space)){
+            if (velocity.magnitude > 0) {
+                animator.SetBool("is_running", true);
+                transform.position += velocity;
+            } else {
+                animator.SetBool("is_running", false);
+            }
+
+            //Jump
+            if (Input.GetKey(KeyCode.Space)) {
                 rb.AddForce(new Vector3(0, thrust, 0));
                 f_ground = false;
                 //Debug.Log("ground:false");
@@ -57,7 +75,41 @@ public class UnityChanDemo1 : MonoBehaviour {
                 animator.SetBool("is_jumping", false);
             }
         }
-	}
+
+        //Rotation
+        // 左クリックした時
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("左クリック");
+            // カメラの角度を変数"newAngle"に格納
+            //newAngle = campos.transform.localEulerAngles;
+            newAngle = transform.localEulerAngles;
+            // マウス座標を変数"lastMousePosition"に格納
+            lastMousePosition = Input.mousePosition;
+        }
+        // 左ドラッグしている間
+        else if (Input.GetMouseButton(0)) {
+            //カメラ回転方向の判定フラグが"true"の場合
+            if (!reverse) {
+                //Debug.Log("ドラッグ");
+                // Y軸の回転：マウスドラッグ方向に視点回転
+                // マウスの水平移動値に変数"rotationSpeed"を掛ける
+                //（クリック時の座標とマウス座標の現在値の差分値）
+                newAngle.y -= (lastMousePosition.x - Input.mousePosition.x) * rotationSpeed.y;
+                // X軸の回転：マウスドラッグ方向に視点回転
+                // マウスの垂直移動値に変数"rotationSpeed"を掛ける
+                //（クリック時の座標とマウス座標の現在値の差分値）
+                newAngle.x -= (Input.mousePosition.y - lastMousePosition.y) * rotationSpeed.x;
+                // "newAngle"の角度をカメラ角度に格納
+                //campos.transform.localEulerAngles = newAngle;
+                Debug.Log("Mouse Position: " + Input.mousePosition);
+                Debug.Log("Last Position: " + lastMousePosition);
+                Debug.Log("new Angle: " + newAngle);
+                transform.localEulerAngles = newAngle;
+                // マウス座標を変数"lastMousePosition"に格納
+                lastMousePosition = Input.mousePosition;
+            }
+        }
+    }
 
     private void OnCollisionStay(Collision collision) {
         f_ground = true;
